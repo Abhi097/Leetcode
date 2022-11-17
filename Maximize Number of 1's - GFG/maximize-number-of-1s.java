c49a56d90d04e221bb70b7f7f239fba50c38b907
Abhishek Kumar <88137437+Abhi097@users.noreply.github.com>
2022-11-17 20:08:19 +0530

    Added solution - LeetHub

---
+++ Maximize Number of 1's - GFG/maximize-number-of-1s.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+            int m = Integer.parseInt(br.readLine().trim());
+
+            int ans = new Solve().findZeroes(arr, n, m);
+
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solve {
+    // m is maximum of number zeroes allowed to flip
+    int findZeroes(int arr[], int n, int m) {
+        int i=0,j=0,zerocnt=0,ans=0;
+        while(j<n){
+            if(arr[j]==0){
+               zerocnt++; 
+            }
+            while(zerocnt>m){
+                if(arr[i]==0){
+                    zerocnt--;
+                }
+                i++;
+            }
+            ans= Math.max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+}
