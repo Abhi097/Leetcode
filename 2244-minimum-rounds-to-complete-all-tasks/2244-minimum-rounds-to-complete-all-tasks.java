9562e4b0fe02c2d190ab766b4315a281a6f9ca8b
Abhishek Kumar <88137437+Abhi097@users.noreply.github.com>
2023-01-04 18:34:50 +0530

    Time: 67 ms (53.22%), Space: 109.9 MB (12.00%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumRounds(int[] tasks) {
+        HashMap<Integer,Integer>count= new HashMap<>();
+        
+        for(int a:tasks){
+           count.put(a,count.getOrDefault(a,0)+1);
+            
+        }
+        int ans=0;
+        for(int freq:count.values()){
+            if(freq==1)return -1;
+            ans+=(freq+2)/3;
+        }
+        return ans;
+    }
+}
