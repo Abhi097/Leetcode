3ce16006f4012c9147be2bc9615993c6de15691f
Abhishek Kumar <88137437+Abhi097@users.noreply.github.com>
2023-05-06 17:18:39 +0530

    Time: 6 ms (92.55%), Space: 54.7 MB (8.69%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,12 +1,33 @@
 class Solution {
+    private int findQuickSelect(int []nums,int left,int right,int k){
+        if (left == right) return nums[left];
+
+	int pIndex = new Random().nextInt(right - left + 1) + left;
+	pIndex = partition(nums, left, right, pIndex);
+
+	if (pIndex == k) return nums[k];
+	else if (pIndex < k) return findQuickSelect(nums, pIndex+1, right, k);
+	return findQuickSelect(nums, left, pIndex-1, k);
+    }
+    
+    private int partition(int[] nums, int left, int right, int pIndex) {
+	int pivot = nums[pIndex];
+	swap(nums, pIndex, right);
+	pIndex = left;
+
+	for (int i=left; i<=right; i++) 
+		if (nums[i] <= pivot) 
+			swap(nums, i, pIndex++);
+
+	return pIndex - 1;
+}
+    
+    private void swap( int nums[],int i,int j){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer>queue=new PriorityQueue<>(Comparator.reverseOrder());
-        for(int i=0;i<nums.length;i++){
-            queue.add(nums[i]);
-        }
-       for(int i=1;i<k;i++){
-           queue.poll();
-       }
-        return queue.peek();
+       return findQuickSelect(nums,0,nums.length-1,nums.length-k);
     }
 }
